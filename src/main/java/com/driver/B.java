9defc28ff5950a1be1c8ae11bf68946dbde10f4f
Soham <sohamjpathak@gmail.com>
2023-11-04 01:35:31 +0530

    first commit

--- src/main/java/com/driver/B.java
+++
@@ -1,8 +0,0 @@
-package com.driver;
-
-public class B extends A{
-
-    public void meth(){
-        System.out.println("method is overriden in class B");
-    }
-}
--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,6 +1,18 @@
 package com.driver;
 
 public class Main {
+
+    public static class A {
+        public void meth(){
+            System.out.println("Invoking method from class A");
+        }
+    }
+    public static class B extends A{
+
+        public void meth(){
+            System.out.println("method is overriden in class B");
+        }
+    }
     public static void main(String[] agrs){
         A a=new B();
 
